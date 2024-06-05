b799cf64b9dde4d7aeb0cf8456733a28c1354bd7
Dmitry Orlov <orlow.dmitriy36@gmail.com>
2024-06-05 19:20:57 +0300

    feat: added file uploading

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,16 +8,27 @@ export class AuthService {
   private readonly lcKey = 'authData';
 
   public isAuth(): boolean {
-    const authDataRaw = localStorage.getItem(this.lcKey);
-    if (!authDataRaw) {
+    const authData = this.getAuthData();
+    if (!authData) {
       return false;
     }
-    const authData = JSON.parse(authDataRaw) as AuthData;
     return Date.now() < (authData.authAt + this.tokenExpiringTime);
+  }
+
+  public getToken(): string {
+    return this.getAuthData()?.token as string;
   }
 
   public setAuth(token: string): void {
     window.localStorage.setItem(this.lcKey, JSON.stringify({ token, authAt: Date.now() }));
+  }
+
+  private getAuthData(): AuthData | null {
+    const authDataRaw = window.localStorage.getItem(this.lcKey);
+    if (!authDataRaw) {
+      return null;
+    }
+    return JSON.parse(authDataRaw);
   }
 }
 
